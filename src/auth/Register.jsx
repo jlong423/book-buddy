198d78a619dc4cd416eae38eda8be6acbcaf0f2e
Justin Long <cordorrow2@yahoo.com>
2026-04-07 11:45:11 -0500

    Register page CSS added and completed.

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -26,28 +26,31 @@ export default function Register() {
 
   return (
     <>
-      <h1>Register for an account</h1>
-      <form action={tryRegister}>
-        <label>
-          First Name
-          <input type="text" name="first name" required />
-        </label>
-        <label>
-          Last Name
-          <input type="text" name="last name" />
-        </label>
-        <label>
-          Email
-          <input type="text" name="email" required />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" required />
-        </label>
-        <button>Register</button>
-        {error && <p role="alert">{error}</p>}
-      </form>
-      <Link to="/login">Already have an account? Log in here</Link>
+      <div className="register-container">
+        <h2>Register for an account</h2>
+        <form action={tryRegister}>
+          <label>
+            First Name
+            <input type="text" name="first name" required />
+          </label>
+          <label>
+            Last Name
+            <input type="text" name="last name" />
+          </label>
+          <label>
+            Email
+            <input type="text" name="email" required />
+          </label>
+          <label>
+            Password
+            <input type="password" name="password" required />
+          </label>
+          <button>Register</button>
+          {error && <p role="alert">{error}</p>}
+        </form>
+        <br />
+        <Link to="/login">Already have an account? Log in here</Link>
+      </div>
     </>
   );
 }
